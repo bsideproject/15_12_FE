8708688f126640a6346a6622d8d21ee9027465e1
choi-sus <choi-sus@naver.com>
2023-05-25 14:16:12 +0900

    [Remove] : api test module 삭제

--- app/api/test/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-// eslint-disable-next-line import/prefer-default-export
-export async function POST() {
-	return NextResponse.json({ error: 'err' }, { status: 403 });
-}
