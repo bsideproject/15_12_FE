4093a18a0b99d37beb45dc765d7f0a0b6481606b
bbahna <hyoon.dev@gmail.com>
2023-06-02 13:22:51 +0900

    [Feat] : 구글 사용자 정보 endpoint 추가
    - localStorage에 user email 저장
    - 로그아웃 클릭 시, localStorage에 token, email 삭제

--- hooks/useGoogleLogin.ts
+++ hooks/useGoogleLogin.ts
@@ -1,15 +1,32 @@
 'use client';
 
+import axios from 'axios';
 import { useEffect } from 'react';
 
-import apiClient from '@/core';
+// import apiClient from '@/core';
 import useNavigation from '@/hooks/useNavigation';
 
 const useGoogleLogin = (code: string | null) => {
 	const navigation = useNavigation();
-
 	useEffect(() => {
-		apiClient
+		const getGoogleUserInfo = (access_token: string) => {
+			axios
+				.get(`https://${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/oauth2/userInfo`, {
+					headers: {
+						Authorization: `Bearer ${access_token}`,
+					},
+				})
+				.then((res) => {
+					const { email } = res.data;
+					localStorage.setItem('email', email);
+					navigation.push('/dashboard');
+				})
+				.catch((err) => {
+					console.log('구글 사용자 정보 에러..', err);
+				});
+		};
+
+		axios
 			.post(
 				`https://${process.env.NEXT_PUBLIC_COGNITO_DOMAIN}/oauth2/token`,
 				{
@@ -29,7 +46,7 @@ const useGoogleLogin = (code: string | null) => {
 				const ACCESS_TOKEN = res.data.access_token;
 				localStorage.setItem('token', ACCESS_TOKEN);
 				alert('구글 로그인 성공!');
-				navigation.push('/dashboard');
+				getGoogleUserInfo(ACCESS_TOKEN);
 			})
 			.catch((err) => {
 				console.log('구글 로그인 에러..', err);
