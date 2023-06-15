c6d5d7f8ef97b784666c84150c9cac85155bc0b6
choi-sus <choi-sus@naver.com>
2023-06-15 13:54:16 +0900

    [Design] : 전역 layout css 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<div className="layout">{children}</div>
+			</body>
 		</html>
 	);
 }
