1a588203bdb4dce936241aa4805bbe64dc289ebe
choi-sus <choi-sus@naver.com>
2023-06-15 15:17:32 +0900

    [Feat] : tailwind class 합병 Fn 추가

--- components/screen/Splash.tsx
+++ components/screen/Splash.tsx
@@ -1,8 +1,13 @@
 'use client';
 
+import Logo from 'public/images/splash-logo.svg';
+
 export default function ScreenSplash() {
 	return (
 		<div>
+			<div>
+				<Logo />
+			</div>
 			<button type="button">시작하기</button>
 		</div>
 	);
