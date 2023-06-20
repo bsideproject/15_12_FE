641b9ef8ad8ee95c86364a74ac74094fc01d4e0b
choi-sus <choi-sus@naver.com>
2023-06-20 16:14:00 +0900

    [Design] : sidebar animation 및 style 추가

---
+++ components/modules/Sidebar.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { Auth } from 'aws-amplify';
+import { useEffect } from 'react';
+
+import useNavigation from '@/hooks/useNavigation';
+import clsxm from '@/service/mergeStyle';
+import Close from 'public/images/back-icon.svg';
+
+interface SidebarProps {
+	isSidebar: boolean;
+	handleToggleSide: () => void;
+	userName: string | boolean;
+}
+
+export default function Sidebar({ isSidebar, handleToggleSide, userName }: SidebarProps) {
+	const navigation = useNavigation();
+
+	useEffect(() => {
+		if (isSidebar) {
+			document.body.style.cssText = `
+          position: fixed; 
+          top: -${window.scrollY}px;
+          overflow-y: scroll;
+          width: 100%;`;
+		}
+		return () => {
+			const scrollY = document.body.style.top;
+			document.body.style.cssText = '';
+			window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+		};
+	}, [isSidebar]);
+
+	/**
+	 * 로그아웃
+	 */
+	const logout = async () => {
+		try {
+			await Auth.signOut();
+			navigation.push('/');
+		} catch (error) {
+			console.log('error signing out: ', error);
+		}
+	};
+
+	const sidebarClasses = clsxm(
+		`flex flex-col justify-between absolute top-0 right-0 w-[75%] h-screen bg-white ease-in-out duration-300 pt-[13.33%] pb-[8.33%] px-[4.44%] ${
+			isSidebar ? 'translate-x-0 visible' : 'translate-x-full invisible'
+		}`,
+	);
+	const menuClasses = clsxm('text-gray090 mb-[8.88%]');
+	const copylightClasses = clsxm('block text-[#828282] leading-[1.0625rem]');
+
+	return (
+		<aside className={sidebarClasses}>
+			<div>
+				<button type="button" onClick={handleToggleSide} className="mb-[20.17%]">
+					<Close />
+				</button>
+				<ul className="ml-[10.08%]">
+					<li className={`${menuClasses} text-h7 `}>홈</li>
+					<li className={`${menuClasses} text-h7 `}>내 템플릿</li>
+					<li className={`${menuClasses} text-h7 `}>{!userName ? '로그인/회원가입' : '로그아웃'}</li>
+				</ul>
+			</div>
+			<div>
+				<h2 className="text-h7 text-gray090 mb-[3.36%]">얼음땡 팀</h2>
+				<span className={`${copylightClasses} text-c`}>Copyright ©2023 얼음땡 Inc.</span>
+				<span className={`${copylightClasses} text-c`}>All rights reserved.</span>
+			</div>
+		</aside>
+	);
+}
--- components/screen/Home.tsx
+++ components/screen/Home.tsx
@@ -1,81 +1,56 @@
 'use client';
 
-import { Auth } from 'aws-amplify';
 import React, { useEffect, useState } from 'react';
 
 import ACTIVITY_FIGCAPTION from '@/constants/activityFigcaption';
 import useNavigation from '@/hooks/useNavigation';
 import useQueryActivities from '@/queries/queryFn/useQueryActivities';
 import getUserAttributes from '@/service/getUserAttributes';
-import getUserSession from '@/service/getUserSession';
 import clsxm from '@/service/mergeStyle';
 import Logo from 'public/images/home-logo.svg';
 import ButtonIcon from 'public/images/mytemplate-icon.svg';
 
+import Sidebar from '../modules/Sidebar';
+
 export default function ScreenHome() {
 	const navigation = useNavigation();
 	const [userName, setUserName] = useState<string | boolean>('');
+	const [isSidebar, setIsSidebar] = useState<boolean>(false);
 
 	const handleUserInfo = async () => {
 		const info = await getUserAttributes();
-		console.log(info);
 		setUserName(info.nickname);
-	};
-
-	const test = async () => {
-		const info = await getUserSession();
-
-		console.log(info);
 	};
 
 	useEffect(() => {
 		handleUserInfo();
-		test();
 	}, []);
 
-	// const { data } = useQueryActivities();
-
-	const datatest = [
-		{ activity_id: 1, display_name: '스피드 게임', description: '자유 퀴즈를 만들고 정답을 맞혀보세요' },
-		{
-			activity_id: 2,
-			display_name: '진진자 게임',
-			description: '두 개의 진실과 하나의 거짓을 찾아보세요',
-		},
-		{ activity_id: 3, display_name: '감사 서클', description: '서로에게 감사한 일을 전해보세요' },
-		{ activity_id: 4, display_name: '기분 체크인', description: '참여자의 기분을 점수로 확인해 보세요' },
-		{ activity_id: 5, display_name: '미니 네트워킹', description: '소규모 네트워킹을 진행보세요' },
-		{ activity_id: 6, display_name: '이미지 게임', description: '질문에 가장 가까운 사람을 찾아보세요' },
-	];
-
-	/**
-	 * 로그아웃
-	 */
-	const logout = async () => {
-		try {
-			await Auth.signOut();
-			navigation.push('/');
-		} catch (error) {
-			console.log('error signing out: ', error);
-		}
+	const handleToggleSide = () => {
+		setIsSidebar(!isSidebar);
 	};
+
+	const { data } = useQueryActivities();
 
 	const menuWrapClasses = clsxm('flex flex-col justify-between w-[20px] h-[12px]');
 	const menuClasses = clsxm('block w-full bg-gray090 h-[2px]');
-	const templateBtnClasses = clsxm('flex w-full p-[5.13%] mb-[5.13%] bg-white border border-gray020 rounded');
+	const templateBtnClasses = clsxm('flex w-full p-[5.13%] mb-[5.13%] bg-[#F5F7F8] border border-gray020 rounded');
+	const templateContentClasses = clsxm(
+		'flex justify-between items-center flex-wrap [&>li:not(:nth-child(5),:nth-child(6))]:mb-[10.81%]',
+	);
 
 	return (
-		<section className="p-[6.67%]">
+		<section className="p-[6.67%] relative">
 			<div className="flex items-center justify-between mb-[8%]">
 				<div className="flex justify-between items-center w-[87px]">
 					<Logo />
 					<h1 className="text-h6 text-[#727488]">얼음땡</h1>
 				</div>
-				<div className={menuWrapClasses}>
+				<button type="button" className={menuWrapClasses} onClick={handleToggleSide}>
 					<span className={menuClasses} />
 					<span className={menuClasses} />
 					<span className={menuClasses} />
-				</div>
+				</button>
 			</div>
 			<h2 className="text-h3 text-gray090 mb-[8%]">
 				원하는 액티비티를 <br /> 선택하세요
@@ -84,8 +59,8 @@ export default function ScreenHome() {
 				<ButtonIcon />
 				<p className="text-h7 text-gray090 ml-[2.88%]">내 템플릿 사용하기</p>
 			</button>
-			<ul className="flex justify-between items-center flex-wrap [&>li:not(:nth-child(5),:nth-child(6))]:mb-[10.81%]">
-				{datatest.map((activity) => {
+			<ul className={templateContentClasses}>
+				{data?.map((activity: any) => {
 					return (
 						<li
 							key={activity.activity_id}
@@ -105,18 +80,7 @@ export default function ScreenHome() {
 					);
 				})}
 			</ul>
-			{/* <div>
-				{userName ? (
-					<h2>{`회원 이름: ${userName}`}</h2>
-				) : (
-					<button type="button" onClick={() => navigation.push('/login')}>
-						로그인 안 하셨군요 (로그인 버튼)
-					</button>
-				)}
-			</div>
-			<button type="button" onClick={logout}>
-				로그아웃
-			</button> */}
+			<Sidebar isSidebar={isSidebar} handleToggleSide={handleToggleSide} userName={userName} />
 		</section>
 	);
 }
