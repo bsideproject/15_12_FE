4df3966e5a98897632a16569801a5b98c2efecb7
choi-sus <choi-sus@naver.com>
2023-06-21 13:49:18 +0900

    [Design] : onBoarding 시안에 맞춰 style 재설정

--- components/screen/Splash.tsx
+++ components/screen/Splash.tsx
@@ -13,7 +13,7 @@ export default function ScreenSplash() {
 	return (
 		<div className={sectionClasses}>
 			<div className="text-center mt-[24.04%]">
-				<Logo className="mx-auto" />
+				<Logo className="mx-auto w-[41.67%]" />
 				<h2 className="text-h1 text-gray090 mt-[9.62%] mb-[3.85%]">어색한 사이 이제 끝!</h2>
 				<p className="text-p1 text-gray090">
 					꽁꽁 얼어붙은 어색함을 부숴줄 <br /> 아이스 브레이킹 도구 모음
