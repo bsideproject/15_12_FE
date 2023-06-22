2b2e455c87ad8fb4fc9adffd4d9888f1bd57a7da
bbahna <hyoon.dev@gmail.com>
2023-06-22 13:22:51 +0900

    [Design] : 게임 시작 style 보완
    - 디자인 시안에 따른 수정

--- components/screen/StartGame.tsx
+++ components/screen/StartGame.tsx
@@ -2,6 +2,7 @@
 
 import useNavigation from '@/hooks/useNavigation';
 import clsxm from '@/service/mergeStyle';
+import PersonIcon from 'public/images/person-icon.svg';
 
 export default function ScreenStartGame() {
 	const router = useNavigation();
@@ -16,25 +17,28 @@ export default function ScreenStartGame() {
 		}
 	};
 
-	const headerClasses = clsxm('flex', 'justify-center', 'px-[6.67%]', 'py-[3.33%]');
-	const imageClasses = clsxm('mt-[20.28%]', 'mb-[3.61%]', 'w-[41.67%]', 'h-[41.67%]', 'mx-auto', 'bg-gray030');
+	const headerClasses = clsxm('flex', 'items-center', 'justify-center', 'px-[6.67%]', 'py-[3.33%]');
+	const imageClasses = clsxm('mt-[20.28%]', 'mb-[3.61%]', 'w-[150px]', 'h-[150px]', 'mx-auto', 'bg-gray030');
 	const boldTextClasses = clsxm('mb-[1.11%]', 'font-bold', 'text-center');
-	const normalTextClasses = clsxm('mb-[8.89%]', 'text-center', 'text-gray090', 'text-p2');
+	const normalTextClasses = clsxm('mb-[8.89%]', 'text-center', 'text-gray070', 'text-p2');
 	const inputWrapClasses = clsxm(
 		'bg-white',
+		'px-[6.67%]',
 		'py-[4.17%]',
-		'px-[9.17%]',
-		'mx-[13.61%]',
-		'rounded-[1.39%]',
-		'flex',
-		'items-center',
 		'mt-[2.22%]',
 		'mb-[20.83%]',
+		'mx-[13.61%]',
+		'rounded',
+		'flex',
+		'items-center',
+		'border',
+		'border-blue030',
 	);
 	const inputClasses = clsxm('w-full', 'text-p1', 'leading-[4.44%]', 'outline-none', 'text-gray070');
-	const copyClasses = clsxm('leading-[4.44%]', 'text-h6', 'ml-[10px]', 'break-keep', 'text-p2');
-	const participantClasses = clsxm('font-bold', 'text-center');
-	const bottomClasses = clsxm('fixed', 'bottom-0', 'flex', 'w-full', 'max-w-[480px]', 'p-[6.67%]', 'bg-gray010');
+	const copyClasses = clsxm('leading-[4.44%]', 'ml-[2.78%]', 'break-keep', 'text-p2', 'text-blue050');
+	const participantWrapClasses = clsxm('flex', 'items-center', 'justify-center');
+	const participantClasses = clsxm('font-bold', 'text-center', 'text-gray070', 'ml-[2.22%]');
+	const bottomClasses = clsxm('fixed', 'bottom-0', 'flex', 'w-full', 'max-w-[480px]', 'p-[6.67%]');
 	const buttonClasses = clsxm('bg-blue050', 'text-button', 'rounded', 'flex-1');
 
 	return (
@@ -51,7 +55,10 @@ export default function ScreenStartGame() {
 					복사
 				</button>
 			</div>
-			<p className={participantClasses}>참여자 00/00</p>
+			<div className={participantWrapClasses}>
+				<PersonIcon />
+				<p className={participantClasses}>참여자 00/00</p>
+			</div>
 			<button type="button" className={bottomClasses}>
 				시작하기
 			</button>
