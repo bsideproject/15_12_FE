47cb7606be0917ff852843d0e431142dde926fba
bbahna <hyoon.dev@gmail.com>
2023-06-28 13:22:51 +0900

    [Feat] : 스피드 게임 방 조회 rest api 연결
    - 파라미터로 전달받은 roomCode 값으로 방 조회
    - 큐알코드, 방 주소, 참여자 정보 연결

--- components/screen/StartGame.tsx
+++ components/screen/StartGame.tsx
@@ -1,23 +1,49 @@
 'use client';
 
-import useNavigation from '@/hooks/useNavigation';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import apiClient from '@/core';
 import useNotify from '@/hooks/useNotify';
 import clsxm from '@/service/mergeStyle';
 import PersonIcon from 'public/images/person-icon.svg';
 
+interface IRoomProps {
+	participant_count: number;
+	qr_code_image_url: string;
+	room_code: string;
+	room_name: string;
+	short_url: string;
+}
+
 export default function ScreenStartGame() {
-	const router = useNavigation();
+	const searchParams = useSearchParams();
+	const roomCode = searchParams.get('roomCode');
 	const toast = useNotify();
-	const currentUrl = router.path;
+	const [roomData, setRoomData] = useState<IRoomProps>();
 
 	const copyUrl = async () => {
 		try {
-			await navigator.clipboard.writeText(currentUrl());
+			await navigator.clipboard.writeText(`${roomData?.short_url}`);
 			toast.success('링크를 복사했습니다.');
 		} catch (e) {
 			toast.error('링크 복사에 실패했습니다.');
 		}
 	};
+
+	useEffect(() => {
+		const init = () => {
+			apiClient
+				.get(`${process.env.NEXT_PUBLIC_API_URL}/activity/speedgame/${roomCode}`)
+				.then((res) => {
+					setRoomData(res.data);
+				})
+				.catch((err) => {
+					console.log('err..', err);
+				});
+		};
+		init();
+	}, [roomCode]);
 
 	const headerClasses = clsxm('flex', 'items-center', 'justify-center', 'px-[6.67%]', 'py-[3.33%]');
 	const imageClasses = clsxm('mt-[20.28%]', 'mb-[3.61%]', 'w-[150px]', 'h-[150px]', 'mx-auto', 'bg-gray030');
@@ -48,18 +74,18 @@ export default function ScreenStartGame() {
 			<header className={headerClasses}>
 				<h2 className="text-h2">우리 같이 얼음땡 해요!</h2>
 			</header>
-			<div className={imageClasses} />
+			<img src={roomData?.qr_code_image_url} alt="큐알_코드" className={imageClasses} />
 			<p className={boldTextClasses}>입장을 위한 QR코드</p>
 			<p className={normalTextClasses}>QR코드를 스캔해주세요.</p>
 			<div className={inputWrapClasses}>
-				<input className={inputClasses} value={`https://beside1512.dev${currentUrl()}`} disabled />
+				<input className={inputClasses} value={`${roomData?.short_url}`} disabled />
 				<button type="button" className={copyClasses} onClick={copyUrl}>
 					복사
 				</button>
 			</div>
 			<div className={participantWrapClasses}>
 				<PersonIcon />
-				<p className={participantClasses}>참여자 00/00</p>
+				<p className={participantClasses}>참여자 {roomData?.participant_count}명</p>
 			</div>
 			<button type="button" className={bottomClasses}>
 				시작하기
