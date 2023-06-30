667d96fa264e3bef7059b42fb627bfe04ecdb60c
bbahna <hyoon.dev@gmail.com>
2023-06-30 17:13:08 +0900

    [Style] : 아이콘 input element 추가
    - 게임 문제/답안 입력란

---
+++ components/elements/EIIconInput.tsx
@@ -0,0 +1,23 @@
+import { ChangeEventHandler, MouseEventHandler, ReactNode } from 'react';
+
+import clsxm from '@/service/mergeStyle';
+
+interface EIIconInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	icon?: ReactNode;
+	placeholder: string;
+	_onChange?: ChangeEventHandler<HTMLInputElement>;
+	_onDoubleClick?: MouseEventHandler<HTMLDivElement>;
+	answer?: string | boolean;
+}
+
+export default function EIIconInput({ icon, placeholder, _onChange, _onDoubleClick, answer }: EIIconInputProps) {
+	const inputWrapClasses = clsxm('px-[6.67%]', 'py-[4.17%]', 'rounded', 'flex', 'items-center', 'mt-[2.22%]', 'border');
+	const inputClasses = clsxm('w-full', 'text-p2', 'outline-none');
+
+	return (
+		<div className={`${inputWrapClasses} ${answer}`} onDoubleClick={_onDoubleClick}>
+			{icon}
+			<input className={`${inputClasses} ${answer}`} placeholder={placeholder} onChange={_onChange} />
+		</div>
+	);
+}
