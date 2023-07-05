156f2f8e79ef24484c411d26caab636526bcded0
choi-sus <choi-sus@naver.com>
2023-07-05 14:47:27 +0900

    [Design] : 기분 체크인 방만들기 style 적용

--- components/modules/ActivityHead.tsx
+++ components/modules/ActivityHead.tsx
@@ -1,12 +1,17 @@
 'use client';
 
+import useNavigation from '@/hooks/useNavigation';
 import Close from 'public/images/close-icon.svg';
 
 export default function ActivityHead({ title }: { title: string }) {
+	const navigation = useNavigation();
+
 	return (
-		<div>
-			<h3>{title}</h3>
-			<Close />
+		<div className="flex justify-between items-center">
+			<h3 className="text-h3 text-gray090">{title}</h3>
+			<button type="button" onClick={() => navigation.replace('/home')}>
+				<Close />
+			</button>
 		</div>
 	);
 }
--- components/screen/MoodCheckin.tsx
+++ components/screen/MoodCheckin.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import MooodCheckinImg from 'public/images/mood-checkin-img.svg';
+
 import ElButton from '../elements/ElButton';
 import ElGrid from '../elements/ElGrid';
 import ActivityHead from '../modules/ActivityHead';
@@ -8,6 +10,14 @@ export default function ScreenMoodCheckin() {
 	return (
 		<ElGrid between bottomSm>
 			<ActivityHead title="기분 체크인" />
+			<div className="text-center mx-auto">
+				<MooodCheckinImg className="mb-[11.22%]" />
+				<p className="text-p2 text-gray090">
+					참여자의
+					<br />
+					오늘의 기분을 알아보세요!
+				</p>
+			</div>
 			<ElButton type="button">시작하기</ElButton>
 		</ElGrid>
 	);
