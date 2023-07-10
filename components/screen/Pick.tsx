3900b1baa56a94416732afaf8a5c290a2f46c390
choi-sus <choi-sus@naver.com>
2023-07-10 11:54:59 +0900

    [Design] : 기분 체크인 참가자 기분 체크 페이지 style 적용

--- components/screen/Pick.tsx
+++ components/screen/Pick.tsx
@@ -9,13 +9,16 @@ import MoodCheckin03 from 'public/images/mood-checkin-icon03.svg';
 import MoodCheckin04 from 'public/images/mood-checkin-icon04.svg';
 import MoodCheckin05 from 'public/images/mood-checkin-icon05.svg';
 
-import EIIconInput from '../elements/EIIconInput';
 import ElButton from '../elements/ElButton';
 import ElGrid from '../elements/ElGrid';
 import ActivityHead from '../modules/ActivityHead';
 
 export default function ScreenPick() {
 	const [moodNum, setMoodNum] = useState<string>('');
+
+	const onChangMood = (value: string) => {
+		setMoodNum(value);
+	};
 
 	const iconArr: React.ReactElement<React.SVGProps<SVGSVGElement>>[] = [
 		<MoodCheckin01 key="MoodCheckin01" />,
@@ -26,31 +29,45 @@ export default function ScreenPick() {
 	];
 
 	return (
-		<ElGrid between bottomSm>
-			<div>
-				<ActivityHead title="기분 체크인" />
-				<p className="text-p2 text-gray070 mt-[4.49%] mb-[13.46%]">오늘의 기분을 말해주세요</p>
-				<Logo className="mx-auto mb-[13.62%]" />
-				<div>
+		<ElGrid autoHeight pxNone bottomSm>
+			<div className="mb-[8.89%]">
+				<div className="px-[6.67%]">
+					<ActivityHead title="기분 체크인" />
+					<p className="text-p2 text-gray070 mt-[4.49%] mb-[13.46%]">오늘의 기분을 말해주세요</p>
+					<Logo className="mx-auto mb-[13.62%]" />
+				</div>
+				<ul className="bg-gray000 border border-gray020 px-[6.70%] py-[6.15%] [&>li:not(:last-child)]:mb-[1%]">
 					{iconArr.map((el, i) => {
 						return (
-							<EIIconInput
-								key={`${el.key}`}
-								icon={el}
-								value={`${i}점`}
-								_onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-									setMoodNum(e.target.value);
-								}}
-								_onDoubleClick={() => {
-									console.log('s');
-								}}
-								answer={moodNum === `${i}점` ? 'border-blue050 text-blue050' : 'border-gray020'}
-							/>
+							<li key={`${el.key}`}>
+								<button
+									type="button"
+									onClick={() => onChangMood(`${i + 1}점`)}
+									className={`border border-gray020 rounded px-[5.84%] py-[2.92%] w-full flex items-center ${
+										moodNum === `${i + 1}점`
+											? '!border-blue050 text-h7 text-blue050'
+											: 'border-gray020 text-p2 text-gray020'
+									}`}
+								>
+									<div
+										className={`mr-[8px] ${
+											moodNum === `${i + 1}점`
+												? '[&>svg>path]:fill-blue050 [&>svg>g>path]:fill-blue050'
+												: '[&>svg>path]:fill-gray020 [&>svg>g>path]:fill-gray020'
+										}`}
+									>
+										{el}
+									</div>
+									{`${i + 1}점`}
+								</button>
+							</li>
 						);
 					})}
-				</div>
+				</ul>
 			</div>
-			<ElButton type="button">제출하기</ElButton>
+			<div className="px-[6.67%]">
+				<ElButton type="button">제출하기</ElButton>
+			</div>
 		</ElGrid>
 	);
 }
