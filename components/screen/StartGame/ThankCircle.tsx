5af954978f92927bd13ee226033bc0652de90f5f
choi-sus <choi-sus@naver.com>
2023-07-11 09:26:38 +0900

    [Design] : 감사서클 타이틀 page style 추가

--- components/screen/StartGame/ThankCircle.tsx
+++ components/screen/StartGame/ThankCircle.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import { useEffect } from 'react';
+
 import StartTemplate from '@/components/modules/StartTemplate';
 import useNavigation from '@/hooks/useNavigation';
-import useQueryThankCircle from '@/queries/queryFn/useQueryThankCircle';
+import useTest from '@/hooks/useTest';
+import useQueryMoodCheckin from '@/queries/queryFn/useQueryMoodCheckin';
+import getUserSession from '@/service/getUserSession';
 
 export default function StartThankCircle() {
 	const navigation = useNavigation();
@@ -10,7 +14,19 @@ export default function StartThankCircle() {
 	const activity = navigation.path().split('/')[1];
 	const room = navigation.params('room')!;
 
-	const { data } = useQueryThankCircle(activity, room);
+	const { data } = useQueryMoodCheckin(activity, room);
+
+	const { connect, disconnect, payload } = useTest(`/topic/moodcheckin/${data?.room_name}/user-count`);
+
+	const userToken = async () => {
+		const session = await getUserSession();
+		connect({ Autorization: `${session?.getAccessToken().getJwtToken()}` });
+	};
+
+	useEffect(() => {
+		userToken();
+		return () => disconnect();
+	}, [data]);
 
 	return <StartTemplate data={data} activity={activity} room={room} />;
 }
