e0a3471719deab755ae47ab5c1aa5a67bdcd2f2d
choi-sus <choi-sus@naver.com>
2023-07-11 09:34:27 +0900

    [Feat] : 감사 서클 start page 추가

--- components/screen/StartGame/ThankCircle.tsx
+++ components/screen/StartGame/ThankCircle.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 import StartTemplate from '@/components/modules/StartTemplate';
 import useNavigation from '@/hooks/useNavigation';
 import useTest from '@/hooks/useTest';
-import useQueryMoodCheckin from '@/queries/queryFn/useQueryMoodCheckin';
+import useQueryThankCircle from '@/queries/queryFn/useQueryThankCircle';
 import getUserSession from '@/service/getUserSession';
 
 export default function StartThankCircle() {
@@ -14,9 +14,9 @@ export default function StartThankCircle() {
 	const activity = navigation.path().split('/')[1];
 	const room = navigation.params('room')!;
 
-	const { data } = useQueryMoodCheckin(activity, room);
+	const { data } = useQueryThankCircle(activity, room);
 
-	const { connect, disconnect, payload } = useTest(`/topic/moodcheckin/${data?.room_name}/user-count`);
+	const { connect, disconnect, payload } = useTest(`/topic/thankcircle/${data?.room_name}/user-count`);
 
 	const userToken = async () => {
 		const session = await getUserSession();
