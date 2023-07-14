12cf5f9f5f39a83621f4ea79e1cfd33d919f11bf
bbahna <hyoon.dev@gmail.com>
2023-07-14 21:13:08 +0900

    [Design] : header 오른쪽 custom 가능하도록 추가

--- components/modules/ActivityHead.tsx
+++ components/modules/ActivityHead.tsx
@@ -1,17 +1,21 @@
 'use client';
 
+import { ReactNode } from 'react';
+
 import useNavigation from '@/hooks/useNavigation';
 import Close from 'public/images/close-icon.svg';
 
-export default function ActivityHead({ title }: { title: string }) {
+export default function ActivityHead({ title, right }: { title: string; right?: ReactNode }) {
 	const navigation = useNavigation();
 
 	return (
-		<div className="flex justify-between items-center">
+		<div className="flex items-center justify-between">
 			<h3 className="text-h3 text-gray090">{title}</h3>
-			<button type="button" onClick={() => navigation.replace('/home')}>
-				<Close />
-			</button>
+			{right || (
+				<button type="button" onClick={() => navigation.replace('/home')}>
+					<Close />
+				</button>
+			)}
 		</div>
 	);
 }
