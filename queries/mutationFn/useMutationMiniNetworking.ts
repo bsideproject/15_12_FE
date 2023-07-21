bd491ac5f8c973a1f9e07f901989b7bcd5e589aa
choi-sus <choi-sus@naver.com>
2023-07-21 20:18:39 +0900

    [Feat] : 주최자 진행 socket mutation에 적용

--- queries/mutationFn/useMutationMiniNetworking.ts
+++ queries/mutationFn/useMutationMiniNetworking.ts
@@ -1,14 +1,35 @@
 import { useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import { useSetRecoilState } from 'recoil';
 
+import { useDisconnect, usePublish } from '@/atoms/socketAtoms';
 import useNavigation from '@/hooks/useNavigation';
+import useSocket from '@/hooks/useSocket';
+import getUserSession from '@/service/getUserSession';
 
 import apiKeys from '../apiKeys';
 
 const useMutationMiniNetworking = () => {
 	const navigation = useNavigation();
 
+	const setPublish = useSetRecoilState(usePublish);
+	const setDisconnect = useSetRecoilState(useDisconnect);
+
+	const { connect, publish, disconnect } = useSocket();
+
+	const connectOrganizer = async (data: AxiosResponse<any, any>) => {
+		const session = await getUserSession();
+
+		connect(`mininetworking/${data?.data.room_name}`, { Authorization: `${session?.getAccessToken().getJwtToken()}` });
+	};
+
 	const { mutate } = useMutation(apiKeys.createMiniNetworking, {
 		onSuccess: (data) => {
+			connectOrganizer(data);
+
+			setPublish(() => publish);
+			setDisconnect(() => disconnect);
+
 			navigation.push(`/mininetworking/start-game/${data?.data.room_name}`);
 		},
 		onError: (error) => {
